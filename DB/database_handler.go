3b5299ffa9fec126da627771fd401f7bc7427b20
Maksud <leylakarimova@mail.ru>
2023-03-19 13:41:40 +0300

    Added variables to handle db

---
+++ DB/database_handler.go
@@ -0,0 +1,15 @@
+package db
+
+import (
+	"fmt"
+	"os"
+)
+
+var host = os.Getenv("HOST")
+var port = os.Getenv("PORT")
+var user = os.Getenv("USER")
+var password = os.Getenv("PASSWORD")
+var dbname = os.Getenv("DBNAME")
+var sslmode = os.Getenv("SSLMODE")
+
+var dbInfo = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, dbname, sslmode)
