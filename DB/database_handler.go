1469f83e4ec5689b198aaed62a698a9e7276dd80
Maksud <leylakarimova@mail.ru>
2023-03-19 13:51:41 +0300

    Added database

--- DB/database_handler.go
+++ DB/database_handler.go
@@ -1,8 +1,10 @@
 package db
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var host = os.Getenv("HOST")
@@ -13,3 +15,40 @@ var dbname = os.Getenv("DBNAME")
 var sslmode = os.Getenv("SSLMODE")
 
 var dbInfo = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, dbname, sslmode)
+
+func collectData(username string, chatid int64, message string, answer []string) error {
+	db, err := sql.Open("postgres", dbInfo)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	answ := strings.Join(answer, ", ")
+
+	data := `INSERT INTO users(username, chat_id, message, answer) VALUES($1, $2, $3, $4);`
+
+	if _, err = db.Exec(data, `@`+username, chatid, message, answ); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getNumberOfUsers() (int64, error) {
+
+	var count int64
+
+	db, err := sql.Open("postgres", dbInfo)
+	if err != nil {
+		return 0, err
+	}
+	defer db.Close()
+
+	row := db.QueryRow("SELECT COUNT(DISTINCT username) FROM users;")
+	err = row.Scan(&count)
+	if err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
